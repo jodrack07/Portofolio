4442e8f3633fa91fa65c792497c88fd5e37a716e
jodrack <drack.sir01@gmail.com>
2022-01-24 15:38:58 +0200

    Add menu when humber button get clicked

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
+const menuLinks = document.querySelector(".menu__links");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
+  menuLinks.classList.toggle("active");
 });
