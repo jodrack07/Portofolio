cc0e1aff558d41c401989d166880799f35ec83a3
jodrack <drack.sir01@gmail.com>
2022-01-25 09:38:48 +0200

    Fix linters errors

--- main.js
+++ main.js
@@ -1,26 +1,29 @@
-const hamburger = document.querySelector(".hamburger");
-const menuLinks = document.querySelector(".menu__links");
+const hamburger = document.querySelector('.hamburger');
+
+const menuLinks = document.querySelector('.menu__links');
 
 // adding the class active when clicking to the hambuger and menuLinks elements
-hamburger.addEventListener("click", () => {
-  document.querySelectorAll(".bar").forEach((bar) => {
-    if (bar.style.backgroundColor == "white") {
-      bar.style.backgroundColor = "#6070ff";
+
+hamburger.addEventListener('click', () => {
+  document.querySelectorAll('.bar').forEach((bar) => {
+    if (bar.style.backgroundColor === 'white') {
+      bar.style.backgroundColor = '#6070ff';
     } else {
-      bar.style.backgroundColor = "white";
+      bar.style.backgroundColor = 'white';
     }
   });
 
-  hamburger.classList.toggle("active");
-  menuLinks.classList.toggle("active");
-  document.querySelector(".main").classList.toggle("blur");
+  hamburger.classList.toggle('active');
+  menuLinks.classList.toggle('active');
+  document.querySelector('.main').classList.toggle('blur');
 });
 
 // removing the class when clicking to the menu__links-item element
-document.querySelectorAll(".links").forEach((link) => {
-  link.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    menuLinks.classList.remove("active");
-    document.querySelector(".main").classList.toggle("blur");
+
+document.querySelectorAll('.links').forEach((link) => {
+  link.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    menuLinks.classList.remove('active');
+    document.querySelector('.main').classList.toggle('blur');
   });
 });
