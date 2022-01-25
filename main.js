7e2f90ea8ed903b1d84ad4a51d7afb4e31d7f4d3
jodrack <drack.sir01@gmail.com>
2022-01-25 08:40:26 +0200

    Fix hamburger toggle effect and Update the README file

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
-const hamburger = document.querySelector('.hamburger');
-const menuLinks = document.querySelector('.menu__links');
+const hamburger = document.querySelector(".hamburger");
+const menuLinks = document.querySelector(".menu__links");
 
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  menuLinks.classList.toggle('active');
+// adding the class active when clicking to the hambuger and menuLinks elements
+hamburger.addEventListener("click", () => {
+  document.querySelectorAll(".bar").forEach((bar) => {
+    if (bar.style.backgroundColor == "white") {
+      bar.style.backgroundColor = "#6070ff";
+    } else {
+      bar.style.backgroundColor = "white";
+    }
+  });
+
+  hamburger.classList.toggle("active");
+  menuLinks.classList.toggle("active");
+  document.querySelector(".main").classList.toggle("blur");
+});
+
+// removing the class when clicking to the menu__links-item element
+document.querySelectorAll(".links").forEach((link) => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    menuLinks.classList.remove("active");
+    document.querySelector(".main").classList.toggle("blur");
+  });
 });
