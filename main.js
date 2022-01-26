bd82eecdea45f6468702c61b7d8bf64ef9a1c4ba
jodrack <drack.sir01@gmail.com>
2022-01-26 17:02:22 +0200

    Update styles and markup, README.md file

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ const workSections = document.querySelector('.work__sections');
 
 const projectsList = [
   {
-    name: 'Tomic',
+    name: 'Tonic',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
     imageLink: 'img/Snapshoot-Portfolio1.png',
     technologies: ['html', 'css', 'javascript'],
