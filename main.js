01f3f95114c9a2317d9200f4aa77e2385f20a6fb
jodrack <drack.sir01@gmail.com>
2022-01-26 15:16:27 +0200

    Add popup window for mobile and desktop

--- main.js
+++ main.js
@@ -25,3 +25,127 @@ document.querySelectorAll('.links').forEach((link) => {
     openMenu.classList.remove('blur');
   });
 });
+
+// Popup windows for Projects viewing
+const modelContainer = document.querySelector('.modelContainer')
+const project = document.querySelector('.name')
+const description = document.querySelector('.description')
+const company = document.querySelector('.company')
+const role = document.querySelector('.role')
+const year = document.querySelector('.year')
+const imageLink = document.querySelector('.image')
+const langs = document.querySelector('.project__lang')
+const links = document.querySelector('.see-project-buttons')
+const modalContainer = document.querySelector('.modelContainer')
+const work__sections = document.querySelector('.work__sections');
+
+const projectsList = [
+  {
+    name: 'Tomic',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    imageLink: 'img/Snapshoot-Portfolio1.png',
+    technologies: ['html', 'css', 'javascript'],
+    role: 'Backend',
+    company: 'canopi',
+    year: '2015',
+    sourceLink: 'https://github.com/joseph07-drack/Portofolio/',
+    demoLink: 'https://joseph07-drack.github.io/Portofolio/',
+  },
+  {
+    name: 'Multi-post stories',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley',
+    imageLink: 'img/Snapshoot-Portfolio2.png',
+    technologies: ['html', 'css', 'javascript'],
+    role: 'Full Stack Dev',
+    company: 'Facebook',
+    year: '2015',
+    sourceLink: 'https://github.com/joseph07-drack/Portofolio/',
+    demoLink: 'https://joseph07-drack.github.io/Portofolio/',
+  },
+  {
+    name: 'Facebook 360',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley',
+    imageLink: 'img/Snapshoot-Portfolio3.png',
+    technologies: ['html', 'css', 'javascript'],
+    role: 'Backend',
+    company: 'Facebook',
+    year: '2015',
+    sourceLink: 'https://github.com/joseph07-drack/Portofolio/',
+    demoLink: 'https://joseph07-drack.github.io/Portofolio/',
+  },
+  {
+    name: 'Facebook 360',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    imageLink: 'img/Snapshoot-Portfolio4.png',
+    technologies: ['html', 'css', 'javascript'],
+    role: 'Lead developer',
+    company: 'Uber',
+    year: '2015',
+    sourceLink: 'https://github.com/joseph07-drack/Portofolio/',
+    demoLink: 'https://joseph07-drack.github.io/Portofolio/',
+  }
+];
+
+// generating content dynamically from the object
+work__sections.innerHTML = ``;
+for(let i = 0; i < projectsList.length; i++) {
+  let languages = ``;
+   projectsList[i].technologies.forEach((language) => {
+    languages += `
+      <li><a href='' class='lang1' title='${language}' class='lang-2'>${language}</a></li>
+    `;
+  })
+  work__sections.innerHTML += `
+      <div class='works'>
+          <div class='works__image'>
+            <img src='${projectsList[i].imageLink}' alt='work 1' class='img1' />
+          </div>
+          <div class='work__description'>
+            <h2>${projectsList[i].name}</h2>
+            <div class='short_desc'>
+              <h4>${projectsList[i].company}</h4>
+              <span></span>
+              <h4>${projectsList[i].role}</h4>
+              <span></span>
+              <p>${projectsList[i].year}</p>
+            </div>
+            <div class='long_desc'>
+              <p>
+                ${projectsList[i].description}
+              </p>
+
+              <ul class='work__lang'>
+                ${languages}
+              </ul>
+            </div>
+            <button type='button' class='btn btn-enabled project-modal-${i}'>See project</button>
+          </div>
+    </div>
+  `
+}
+
+// showing the popup window once a button get clicked
+for(let i = 0; i < projectsList.length; i++) {
+   document.querySelector(`.project-modal-${i}`).addEventListener('click', (e) => {
+    
+    project.innerHTML = projectsList[i].name
+    imageLink.src = projectsList[i].imageLink
+    role.innerHTML = projectsList[i].role
+    company.innerHTML = projectsList[i].company
+    year.innerHTML = projectsList[i].year
+
+    document.querySelector('.work__sections').classList.add('blur');
+    document.querySelector('.header').classList.add('blur');
+    document.querySelector('.showcase').classList.add('blur');
+    document.querySelector('.contact-form').classList.add('blur');
+    document.querySelector('.about-me').classList.add('blur');
+    modalContainer.classList.add('show');
+      
+  })
+}
+document.querySelector('.closeModal').addEventListener('click', () => {
+  modelContainer.style.display = "none";
+  window.location.reload();
+  document.querySelector('.header').classList.remove('blur');
+  document.querySelector('.showcase').classList.remove('blur');
+})
